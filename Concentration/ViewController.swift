fadb43f2095fb7fe965118e4d01429efdd1ff230
Aleksander Pentchev <alek.pentchev@gmail.com>
2020-04-25 18:29:08 +0200

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Aleksander Pentchev on 25/04/2020.
+//  Copyright © 2020 Aleksander Pentchev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
